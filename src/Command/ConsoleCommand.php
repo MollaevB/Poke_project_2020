1077326e7b24a505c320eebae7f33631f3a7c763
TheoOui <73181894+TheoOui@users.noreply.github.com>
2021-01-10 22:51:12 +0100

    Update ConsoleCommand.php

--- src/Command/ConsoleCommand.php
+++ src/Command/ConsoleCommand.php
@@ -12,7 +12,7 @@ declare(strict_types=1);
  * @copyright Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
  * @link      https://cakephp.org CakePHP(tm) Project
  * @since     3.0.0
- * @license   https://opensource.org/licenses/mit-license.php MIT License
+ * @license   https://opensource.org/licenses/mit-license.php MIT Licenses
  */
 namespace App\Command;
 
